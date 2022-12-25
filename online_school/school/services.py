666fc04e53d1cdcc12bc407e57eec6dd6b142ee4
Maria Shmatko <yaakt@mail.ru>
2022-12-25 20:52:29 +0300

    final version

---
+++ online_school/school/services.py
@@ -0,0 +1,10 @@
+import datetime
+from datetime import date
+
+
+def calc_discount(price, discount: float = 0.12):
+    #birth_date = date.today()
+    #if birth_date == date.today():
+    return str(float(price) / discount)
+    #elif date.today() > birth_date:
+     #   birth_date = datetime(2024, 11, 31)
